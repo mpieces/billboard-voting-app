d6dfccc3ecf80e9fa480263e7cdb963fc49b365e
Marisa Ramon <mer0522@gmail.com>
2021-08-19 19:42:29 -0500

    Implement disabling voting buttons and getting id value

--- app/javascript/controllers/vote_controller.js
+++ app/javascript/controllers/vote_controller.js
@@ -1,11 +1,26 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['thumbsUpButton'];
-  static classes = ['primary'];
+  static targets = ['thumbsUpButton', 'thumbsDownButton'];
+  static classes = ['disabled'];
+  static values = { billboard: Number };
+
+  connect() {}
 
   upvote() {
     console.log('In the upvote method');
-    this.thumbsUpButtonTarget.classList.add(this.primaryClass);
+    this.thumbsUpButtonTarget.classList.add(this.disabledClass);
+    console.log(this.data);
+    // console.log(this.event.dataset.id);
+    var id = this.billboardValue;
+    console.log('ID: ' + id);
+  }
+
+  downvote() {
+    console.log('In the downvote method');
+    this.thumbsDownButtonTarget.classList.add(this.disabledClass);
   }
 }
+
+// make 2 different targets for up and down
+// upon click of up, enable down and vv
