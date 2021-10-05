e64780803d9fb02583252cc04962b347bec8fc62
Marisa Ramon <mer0522@gmail.com>
2021-10-05 13:35:47 -0500

    Stopping point with Stimulus JS: clicking upvote button disables downvote button and vv

--- app/javascript/controllers/vote_controller.js
+++ app/javascript/controllers/vote_controller.js
@@ -10,17 +10,35 @@ export default class extends Controller {
   upvote() {
     console.log('In the upvote method');
     this.thumbsUpButtonTarget.classList.add(this.disabledClass);
+    this.thumbsDownButtonTarget.classList.remove(this.disabledClass);
     console.log(this.data);
     // console.log(this.event.dataset.id);
     var id = this.billboardValue;
     console.log('ID: ' + id);
+
+    var $total = $(this).parent().siblings('.total_votes');
+    var direction = $(this).data('direction');
+    console.log('BB ID: ' + id);
+
+    $.ajax({
+      type: 'POST',
+      url: '/billboards/' + id + '/vote',
+      data: { direction: direction },
+      success: function (response) {
+        // console.log("Total : " + $total.text());
+        // console.log(response);
+        $total.text(response.vote_score);
+        console.log('Vote Score : ' + response.vote_score);
+      },
+    });
   }
 
   downvote() {
     console.log('In the downvote method');
     this.thumbsDownButtonTarget.classList.add(this.disabledClass);
+    this.thumbsUpButtonTarget.classList.remove(this.disabledClass);
   }
 }
 
 // make 2 different targets for up and down
-// upon click of up, enable down and vv
+// upon click of up, enable down and vice versa
